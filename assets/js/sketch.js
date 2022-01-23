cc967236f7858fe69655cc491e5ea9571964fe96
Miffy Chen <mchen15@bu.edu>
2022-01-22 21:25:47 -0500

    upload test v2

--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -1,299 +1,201 @@
+var PLAY = 1;
+var END = 0;
+var gameState = PLAY;
+var invisible_ground;
+var doge,doge_running,doge_collided,dogeImage,ufo,ufo_running,ufo_attack;
+var obstaclesGroup,obstacle1,obstacle2,obstacle3,obstacle4;
+var jumpSound,dieSound,checkpointSound,bgSound,playGameSound;
+var score,restart,restartImage,logo,logoImage;
+var isRestart = 0;
+
+var ground,ground_image,invisible_ground;
+
+function preload(){
+  
+  jumpSound = loadSound("assets/music/mario_jump.mp3")
+  dieSound = loadSound("assets/music/mario_die.mp3")  
+  checkPointSound = loadSound("assets/music/checkPoint.mp3")
+  bgSound = loadSound("assets/music/fdc_theme.mp3")
+  playGameSound = loadSound("assets/music/do-you-wanna-play-a-game.mp3")
+  doge_running=loadImage("assets/img/sprite/cropped_fast.gif");
+  doge_collided=loadImage("assets/img/sprite/better_luck.gif");
+  dogeImage=loadImage("assets/img/sprite/cropped_slow.gif");
+  ufo_running=loadImage("assets/img/fomo_bark.gif");
+  obstacle1=loadImage("assets/img/obstacles/meteorite.gif");
+  obstacle2=loadImage("assets/img/obstacles/aliendog2.gif");
+  obstacle3=loadImage("assets/img/obstacles/obstacle1.png");
+  obstacle4=loadImage("assets/img/obstacles/obstacle4.png");    
+  restartImage=loadImage("assets/img/sprite/play.gif");  
+  logoImage=loadImage("assets/img/bg/bg_banner.png");
+  ground_image=loadImage("assets/img/bg/bg_spacex.png");
+}
+
 function setup() {
-  frameRate(60);
-  createCanvas(400, 400);
+ var myCanvas = createCanvas(1280,600);
+ myCanvas.parent("canvas-container");
+ 
+  //底圖
+  ground=createSprite(width/2,height/2,0,0);
+  ground.shapeColor="white";
+  ground.addImage("ground_image",ground_image);
+  ground.scale=1;
+  //ground.debug=true;
+
+  //地板
+  invisible_ground=createSprite(width/2,550,width,10);
+  invisible_ground.visible=false;
+  //invisible_ground.debug=true;
+
+  //LOGO 
+  logo=createSprite(width/2-30, 70,0,0);
+  logo.scale=0.8;
+  logo.shapeColor="white";
+  logo.addImage("logoImage",logoImage);
+  //logo.debug=true;
+ 
+  //狗勾
+  doge=createSprite(width/3,height/2,30,30);
+  doge.addImage("doge_running",doge_running);
+  doge.addImage("doge_collided",doge_collided);
+  doge.addImage("dogeImage",dogeImage);
+  doge.scale=0.2; 
+  doge.setCollider("rectangle",0,0,doge.width,doge.height)
+  doge.visible = true;
+  //doge.debug=true;
+  //doge.velocityX=2;
   
-  gameState = 0;
-  score = 0;
-  isPlaying = false;
-  defaultTimer = 5;
-  countdownTimer = null;
-  lastObsSpawn = null;
+  //小怪
+  ufo=createSprite(width/5,height/2,30,30);
+  ufo.addImage("ufo_running",ufo_running);
+  ufo.scale=0.2;
+  ufo.visible = true;
+  // ufo.velocityY=-13;
+  // ufo.velocityX=Math.round(random(1,2));
+  //ufo.debug=true;
+
+  //奪魂鋸
+  restart = createSprite(width/2,400);
+  restart.addImage(restartImage);
+  restart.visible = false;
+  restart.scale=0.3;
+  //restart.debug=true;
   
-  playerX = null;
-  playerY = null;
-  initPlayerX = 60;
-  initPlayerY = 300;;
-  
-  DEBUG = false;
-  
-  obSpeed = 0.1;
-  isJumping = false;
-  doubleJumped = false;
-  maxJumpDistance = 120;
-  jumpStrength = 20;
-  
-  
-  obstacles = [];
-  level = 1;
-  
-  gravityStrength = 8;
-  timeUntilSpawn = null;
-  gravity = -9.8;
-  
-  playerVelocity = 0;
-  playerAcceleration = 0;
-  playerSize = 30;
-  obstacleSize = 30;
-  playerHalfSize = playerSize / 2;
-  obstacleHalfSize = obstacleSize / 2;
-  
-  gameStarted = 0;
-  isDead = false;
-  
-  highScores = [];
-}
-
-function initGame() {
-  keyCode = 0;
-  level = 1;
-  timeUntilSpawn = int(random(1, 5 - level)) * 1000;
-  countdownTimer = defaultTimer;
-  score = 0;
-  isDead = false;
-  obstacles = [];
-  playerX = initPlayerX;
-  playerY = initPlayerY;
-  fill(0);
-  textSize(36);
-  textStyle(BOLDITALIC);
-  textAlign(CENTER, CENTER);
-  text('FOMO DOG\nPress Space to Start!', 200, 200);
-  if (DEBUG || keyIsDown(32)) {
-    gameState++;
-  }
-}
-
-function drawHUD() {
-  if (!isDead) {
-    fill(0);
-    textSize(26);
-    textStyle(BOLD);
-    textAlign(LEFT, TOP);
-    text('SCORE: ' + score, 0, 0);
-  }
-
-  rectMode(CORNER);
-  fill(0);
-  rect(0, initPlayerY + 15, width, 100);
-}
-
-function countdown() {
-  if (DEBUG || countdownTimer === 0) {
-    lastObsSpawn = int(millis());
-    gameStarted = int(millis());
-    isPlaying = true;
-  } else {
-    fill(0);
-    textSize(64);
-    textStyle(BOLDITALIC);
-    textAlign(CENTER);
-    text(countdownTimer, 200, 200);
-    if (frameCount % 60 == 0 && countdownTimer > 0) {
-      countdownTimer--;
-    }
-    textSize(18);
-    textStyle(ITALIC);
-    text("Press SPACE to jump over the obstacles\nAnd press again for a double jump", 200, 250);
-  }
-}
-
-function keyPressed() {
-  if (!isDead && gameState === 1 && keyCode === 32) {
-    if (isJumping && !doubleJumped) {
-      playerVelocity = 30;
-      doubleJumped = true;
-      console.log("[SPACE BAR] Double Jump!!");
-    }
-    if (!isJumping) {
-      playerVelocity = 40;
-      isJumping = true;
-      console.log("[SPACE BAR] Jump!");
-    }
-  }
-}
-
-function drawPlayer() {
-  rectMode(CENTER);
-  strokeWeight(3);
-  if (isDead) {
-    fill(255, 0, 0);
-  } else {
-    fill(255);
-  }
-  dt = deltaTime / 100;
-  if (isJumping) {
-    //print("deltaTime: " + dt);
-    playerY -= playerVelocity * dt;
-    playerVelocity += gravity * dt;
-    /*
-    if(keyIsDown(32) && !doubleJumped){
-      playerVelocity += 10;
-      doubledJumped = true;
-    }  
-    */
-    if (playerY >= initPlayerY) {
-      playerY = initPlayerY;
-      playerVelocity = 0;
-      isJumping = false;
-      doubleJumped = false;
-    }
-  }
-
-  rect(playerX, playerY, playerSize);
-
-  /*
-  if(keyIsDown(32) && playerY === initPlayerY){
-    isJumping = true;
-    playerVelocity = 40;
-  } 
-  */
-  /*  
-  if(isJumping && playerY >= initPlayerY - maxJumpDistance){
-    playerY-= jumpStrength;
-  }
-  if (!keyIsDown(32) || playerY <= initPlayerY-maxJumpDistance){
-    isJumping = false;
-  }
-  if(playerY <= initPlayerY) {
-    playerY +=gravityStrength;
-    if(playerY >= initPlayerY) playerY = initPlayerY;
-  }
-  */
-}
-
-function playGame() {
-  if (!isPlaying) {
-    countdown();
-  }
-  else {
-    if (int(millis()) - gameStarted > 10000) {
-      level++;
-      gameStarted = int(millis());
-    }
-    drawHUD();
-    drawPlayer();
-    spawnObstacles();
-    drawObstacles();
-    if (isDead) {
-      fill(255, 0, 0);
-      textAlign(CENTER);
-      textSize(48);
-      textStyle(BOLDITALIC);
-      text("GAME OVER!", 200, 200);
-      if (keyIsDown(32)) {
-        gameState++;
-        console.log("[SPACE BAR] Restart");
-      }
-    }
-    if (detectCollision()) {
-      isDead = true;
-    }
-  }
-}
-
-function detectCollision() {
-  topOfObstacle = initPlayerY - obstacleHalfSize;
-  bottomOfPlayer = playerY + playerHalfSize;
-  playerRightSide = playerX + playerHalfSize;
-  playerLeftSide = playerX - playerHalfSize;
-
-  //  if(isJumping) {
-  //    print("bottomOfPlayer " + bottomOfPlayer + " playerY" + playerY);
-  //    print("topOfObstacle " + topOfObstacle);
-  //  }
-  for (i = 0; i < obstacles.length; i++) {
-    obstacleLeftSide = obstacles[i].x - obstacleHalfSize;
-    obstacleRightSide = obstacles[i].x + obstacleHalfSize;
-    //    if(isJumping) {
-    //    print (i + ") " + obstacleLeftSide + " " + obstacleRightSide + " " + playerLeftSide + " " + playerRightSide);
-    //    }
-    if (obstacleLeftSide <= playerRightSide && obstacleRightSide >= playerLeftSide) {
-      // print("Checking height " + bottomOfPlayer + " >=? " + topOfObstacle + "actual center of playerY is " + playerY);
-      if (bottomOfPlayer >= topOfObstacle) {
-        // print ("XXXXX " + bottomOfPlayer + " " + topOfObstacle);
-        // frameRate(0);
-        return true;
-      }
-    }
-    else {
-      if (obstacleRightSide < playerLeftSide && obstacles[i].scored == false) {
-        score++;
-        obstacles[i].scored = true;
-      }
-      // print ("No overlap yet")
-      if (obstacleLeftSide >= playerRightSide)
-        // this, and all future obstacles in the array are too far away to check
-        return false;
-    }
-  }
-}
-
-function drawObstacles() {
-  if (isDead) {
-    return;
-  }
-  dt = deltaTime / 100;
-  for (i = 0; i < obstacles.length; i++) {
-    rectMode(CENTER);
-    strokeWeight(3);
-    fill(255, 0, 0);
-    rect(obstacles[i].x, initPlayerY, obstacleSize);
-    obstacles[i].x -= (15 * dt) + obstacles[i].speed;  // faster as the game goes on
-  }
-  if (obstacles.length && obstacles[0].x < 0) {
-    obstacles.splice(0, 1);
-  }
-}
-
-function spawnObstacles() {
-  if (isDead) {
-    return;
-  }
-  if (int(millis() - lastObsSpawn) >= timeUntilSpawn) {
-    if (obstacles.length < 8) {
-      obstacles.push({ x: 410, scored: false, speed: int(random(max(1, level - 1), min(level * 2, 20))) })
-      if (level < 5) {
-        timeUntilSpawn = random(1, max(0.1, 2)) * (1000);
-      } else if (level < 8) {
-        timeUntilSpawn = random(1, max(0.1, 2)) * (800);
-      } else if (level < 12) {
-        timeUntilSpawn = random(1, max(0.1, 2)) * (720);
-      } else {
-        timeUntilSpawn = random(1, max(0.1, 2)) * (500 - (level * 2));
-      }
-      lastObsSpawn = millis();
-    }
-  }
-}
-
-function gameOver() {
-  isPlaying = false;
-  fill(0);
-  textAlign(LEFT, TOP);
-  textStyle(BOLD);
-  textAlign(CENTER, CENTER);
-  textSize(32);
-  text("Your Score: " + score, 200, 100);
-  textSize(24);
-  text("CLICK TO RESTART", 200, 200);
-}
-
-function mouseClicked() {
-  if (gameState === 2) {
-    gameState = 0;
-  }
+  obstaclesGroup=new Group();
+  score=0;  
 }
 
 function draw() {
-  background(255);
-  switch (gameState) {
-    case 1:
-      playGame();
-      break;
-    case 2:
-      gameOver();
-      break;
-    default:
-      initGame();
+  clear();
+  
+  // console.log(doge.y);
+  doge.velocityY = doge.velocityY + 0.8;
+  doge.collide(invisible_ground); 
+  
+  ufo.velocityY = ufo.velocityY + 0.8;
+  ufo.collide(invisible_ground); 
+    
+  if (gameState===PLAY){
+
+    //console.log('ground.x:'+ground.x);
+    //ground.velocityX=-1;
+    if (ground.x < 0){
+      ground.x = ground.width/2;
+    }
+    ground.velocityX = -(4 + 3*score/100);
+
+    if (playGameSound.isPlaying()) playGameSound.stop();
+    if (!bgSound.isPlaying()) bgSound.play();
+
+    restart.visible=false;
+    score = score + Math.round(getFrameRate()/60);
+ 
+    spawnObstacles();
+    if (obstaclesGroup.isTouching(ufo)){
+      ufo.velocityY=-12;
+    }
+   
+     if(score>0 && score%100 === 0){
+       checkPointSound.play() 
+    }
+    
+    if (doge.isTouching(obstaclesGroup)){
+      gameState=END;
+      dieSound.play();
+      bgSound.stop();
+       isRestart = 1;
+       ground.velocityX=0;
+    }
+    else
+    {
+      doge.visible=true;
+      ufo.visible=true;        
+    }
+    
+    if(((keyDown("space") || mousePressedOver(doge) ) && doge.y >= 200)) {
+      doge.velocityY = -12;
+      jumpSound.play();
+    }  
   }
+  else if ( gameState===END) {
+
+    setTimeout(function() {
+      if (!playGameSound.isPlaying() && isRestart == 1) playGameSound.play();
+      isRestart = 0;
+      restart.visible=true;    
+      doge.visible=false;
+      ufo.visible=false;    
+      }, 5000);
+
+
+    doge.velocityY = 0
+    doge.changeImage("dogeImage",dogeImage);
+     ufo.x=doge.x;
+     ufo.y=doge.y-110;
+    //if (ufo.isTouching(doge)) {
+      doge.changeImage("doge_collided",doge_collided);
+    //}
+      
+    obstaclesGroup.destroyEach();
+    obstaclesGroup.setLifetimeEach(-1);
+    obstaclesGroup.setVelocityXEach(0);
+  
+    if(mousePressedOver(restart)) {
+      reset();     
+    }
+  } 
+ 
+  drawSprites();
+  fill("white");
+  textSize(20);
+  text("分數: "+ score, width-130,100);
 }
+
+function reset(){
+    gameState=PLAY;
+    restart.visible=false;
+    doge.changeAnimation("doge_running",doge_running);   
+    obstaclesGroup.destroyEach();
+    score=0;
+    doge.visible=true;
+    ufo.x=width/5;    
+    ufo.visible=true;    
+    ufo.y=doge.y;
+}
+
+function spawnObstacles() {
+   if (frameCount % 60 === 0){
+   var obstacle = createSprite(width,550-(Math.random()*350),30,30);
+   obstacle.velocityX = - (6 + score/100);
+    
+   var obstacles = [obstacle1,obstacle2];  
+   var theObstacle = obstacles[Math.floor(Math.random() * obstacles.length)];
+   obstacle.addImage(theObstacle);
+
+   obstacle.scale=0.25;
+   obstaclesGroup.add(obstacle);
+   obstacle.debug=false;
+   obstacle.setCollider("circle",0,0,1);
+  }
+     
+}
+
